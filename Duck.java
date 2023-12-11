f39df1eeacd33ada65815a634592c13f403fec60
100percentnoob <150175971+100percentnoob@users.noreply.github.com>
2023-12-11 13:30:48 -0500

    added a bit of the duck

--- Duck.java
+++ Duck.java
@@ -1,22 +1,21 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class MyWorld here.
+ * Write a description of class Duck here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
  */
-public class MyWorld extends World
+public class Duck extends World
 {
-
+    GreenfootImage idle = new GreenfootImage("images/Duck_idle/tile000.png");
     /**
-     * Constructor for objects of class MyWorld.
+     * Constructor for objects of class Duck.
      * 
      */
-    public MyWorld()
+    public Duck()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
-        Duck duck = new Duck();
+        super(600, 400, 1); 
     }
 }
